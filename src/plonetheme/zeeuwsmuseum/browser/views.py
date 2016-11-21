286a6dd6b2c1d06eed81406954e4c5e2e67a0ef8
andreesg <andreslb1@gmail.com>
2016-11-21 17:20:01 +0100

    Add correct html for online experience

--- src/plonetheme/zeeuwsmuseum/browser/views.py
+++ src/plonetheme/zeeuwsmuseum/browser/views.py
@@ -12,6 +12,100 @@ from plone.app.uuid.utils import uuidToCatalogBrain
 
 class OnlineExperienceView(CollectionView):
 
+    def find_orientation(self, item):
+        if type(item) == str:
+            if item == "L":
+                return "landscape"
+            else:
+                return "portrait"
+
+        item_class = ""
+        if item.portal_type == "Image":
+            image_obj = item.getObject()
+            if getattr(image_obj, 'image', None):
+                try:
+                    w, h = image_obj.image.getImageSize()
+                    if w > h:
+                        item_class = "%s" %('landscape')
+                    else:
+                        item_class = "%s" %('portrait')
+                except:
+                    return item_class
+        elif item.hasMedia:
+            image = uuidToCatalogBrain(item.leadMedia)
+            image_obj = image.getObject()
+            if getattr(image_obj, 'image', None):
+                try:
+                    w, h = image_obj.image.getImageSize()
+                    if w > h:
+                        item_class = "%s" %('landscape')
+                    else:
+                        item_class = "%s" %('portrait')
+                except:
+                    return item_class
+
+        return item_class
+
+    def getPatterns(self, results):
+        # L P L L L P P P
+        TEST_INPUT = ["L", "P", "L", "L", "L", "P", "P", "P"]
+        FIRST_ITEM = 0
+        
+        items = results
+        total_items = len(items)
+        items_checked = []
+        final_patterns = []
+
+        for i in range(total_items):
+            if i not in items_checked:
+
+                pattern = {
+                    "size": "small",
+                    "orientation": self.find_orientation(items[i]),
+                    "position": "pair",
+                    "clearfix": False
+                }
+               
+                if i == FIRST_ITEM:
+                    pattern['position'] = "single"
+                    pattern['size'] = "big"
+                    final_patterns.append(pattern)
+                    items_checked.append(i)
+                else:
+                    if i+1 < total_items:
+                        next_orientation = self.find_orientation(items[i+1])
+
+                        if next_orientation == pattern["orientation"] == "landscape":
+                            pattern["position"] = "single"
+                            pattern["size"] = "big"
+                            final_patterns.append(pattern)
+
+                        else:
+                            new_pattern = {
+                                "size": pattern['size'],
+                                "orientation": pattern['orientation'],
+                                "position": "pair",
+                                "clearfix": True
+                            }
+                            new_pattern["orientation"] = next_orientation
+
+                            if next_orientation == pattern['orientation'] == "portrait":
+                                pattern['size'] = "big"
+                                new_pattern['size'] = "big"
+                            final_patterns.append(pattern)
+                            final_patterns.append(new_pattern)
+                            items_checked.append(i)
+                            items_checked.append(i+1)
+                    else:
+                        pattern['position'] = "single"
+                        pattern['size'] = "big"
+                        final_patterns.append(pattern)
+            else:
+                pass
+
+        return final_patterns
+
+
     def getImageObject(self, item):
         if item.portal_type == "Image":
             return item.getURL()+"/@@images/image/large"
